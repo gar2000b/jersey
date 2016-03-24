e6c51e8b34f9bf74438a0616e668dbdd5382d667
Black <C11951A@us.experian.local>
2016-03-24 09:50:26 -0400

    updated to support JSON / GET and POST

--- simple-service-webapp/src/main/java/com/example/MyResource.java
+++ simple-service-webapp/src/main/java/com/example/MyResource.java
@@ -7,11 +7,15 @@ import java.io.InputStreamReader;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.example.model.Track;
 
 /**
  * Root resource (exposed at "myresource" path)
@@ -54,6 +58,55 @@ public class MyResource {
 			e.printStackTrace();
 		}
 		System.out.println(sb.toString());
+	}
+	
+	// curl: curl -X POST http://localhost:8080/simple-service-webapp/webapi/myresource/abc
+    @POST // post with param (not a large payload)
+    @Path("/{param}")
+    @Consumes(MediaType.TEXT_PLAIN)
+    public Response postMsg(@PathParam("param") String msg) {
+        String output = "POST:Jersey say : " + msg;
+        System.out.println(output);
+        return Response.status(200).entity(output).build();
+    }
+    
+	// curl: curl --data 'some data' http://localhost:8080/simple-service-webapp/webapi/myresource/post
+    @POST // post with a large payload
+    @Path("/post")
+    // @Consumes(MediaType.TEXT_PLAIN)
+    public Response postStrMsg(String msg) {
+        String output = "POST:Jersey say : " + msg;
+        System.out.println(output);
+        return Response.status(200).entity(output).build();
+    }
+    
+    // get example returning JSON
+    // curl: curl -i http://localhost:8080/simple-service-webapp/webapi/myresource/getTrack
+	@GET
+	@Path("/getTrack")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Track getTrackInJSON() {
+
+		Track track = new Track();
+		track.setTitle("Enter Sandman");
+		track.setSinger("Metallica");
+
+		return track;
+
+	}
+    
+    // now create a post that consumes a JSON payload
+	// cd ../instructions
+	// curl: curl -X POST -d @track.txt http://localhost:8080/simple-service-webapp/webapi/myresource/postTrack --header "Content-Type:application/json"
+	@POST
+	@Path("/postTrack")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response createTrackInJSON(Track track) {
+
+		String result = "Track saved : " + track;
+		System.out.println(result);
+		return Response.status(201).entity(result).build();
+		
 	}
 
 }
